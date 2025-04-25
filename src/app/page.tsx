f6c6a6a1875dd5d64559667fa4a50ce0a29338f6
Leodata01 <sebi2409@hotmail.fr>
2025-04-25 09:34:38 +0200

    pas mal de trucs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+import {
+  SignedIn,
+  SignOutButton,
+  SignedOut,
+  SignInButton,
+} from "@clerk/nextjs";
+
 const HomePage = () => {
-  return <div className="flex items-center justify-center">home page</div>;
+  return (
+    <div className="flex items-center justify-center">
+      home page
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </div>
+  );
 };
 
 export default HomePage;
