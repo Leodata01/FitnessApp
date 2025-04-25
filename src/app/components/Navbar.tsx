531047ad3d94f2bc8472cc3d8a334e0a1e5fff65
Leodata01 <sebi2409@hotmail.fr>
2025-04-25 15:08:07 +0200

    nav bar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
+import { ZapIcon, HomeIcon, DumbbellIcon, UserIcon } from "lucide-react";
+import { Button } from "./ui/button";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  const { isSignedIn } = useUser();
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-background/60 backdrop-blur-md border-b border-border py-3">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* LOGO */}
+        <Link href="/" className="flex items-center gap-2">
+          <div className="p-1 bg-primary/10 rounded">
+            <ZapIcon className="w-4 h-4 text-primary" />
+          </div>
+          <span className="text-xl font-bold font-mono">
+            Fitness<span className="text-primary">App</span>.ai
+          </span>
+        </Link>
+
+        {/* Navigation */}
+        <nav className="flex items-center gap-5">
+          {isSignedIn ? (
+            <>
+              <Link
+                href="/"
+                className="flex items-center gap-1.5 text-sm font-medium hover:text-primary transition-colors"
+              >
+                <HomeIcon size={20} />
+                Home
+              </Link>
+
+              <Link
+                href="/generate-program"
+                className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors"
+              >
+                <DumbbellIcon size={17} />
+                <span>Generate</span>
+              </Link>
+
+              <Link
+                href="/profile"
+                className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors"
+              >
+                <UserIcon size={16} />
+                <span>Profile</span>
+              </Link>
+
+              <Button
+                asChild
+                variant="outline"
+                className="ml-2 border-primary/50 text-primary hover:text-white hover:bg-primary/10"
+              >
+                <Link href="/generate-program">Get Started</Link>
+              </Button>
+            </>
+          ) : (
+            <> hello</>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
